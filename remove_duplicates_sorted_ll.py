2a58c8e86f38c3f55809717397516cbb1f792466
karthik-siru <71187119+karthik-siru@users.noreply.github.com>
2021-06-05 19:35:19 +0530

    remove_duplicates from sorted list

---
+++ remove_duplicates_sorted_ll.py
@@ -0,0 +1,34 @@
+
+'''
+
+the main issue is when we have to remove the head of the linkedlist . so we introduce a sentinal and return the sentinal next as the result .
+
+'''
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        # sentinel
+        sentinel = ListNode(0, head)
+
+        # predecessor = the last node 
+        # before the sublist of duplicates
+        pred = sentinel
+        
+        while head:
+            # if it's a beginning of duplicates sublist 
+            # skip all duplicates
+            if head.next and head.val == head.next.val:
+                # move till the end of duplicates sublist
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                # skip all duplicates
+                pred.next = head.next 
+            # otherwise, move predecessor
+            else:
+                pred = pred.next 
+                
+            # move forward
+            head = head.next
+            
+        return sentinel.next
